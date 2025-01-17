fcec5e37b223678c51452a5522f9a184a772a9ba
didier-cost <didier.adams@cost.eu>
2025-01-17 14:23:52 +0100

    Try to replace Spaceless

---
+++ src/Knp/Menu/Twig/SpacelessExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Knp\Menu\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class SpacelessExtension extends AbstractExtension
+{
+    public function __construct()
+    {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('safespaceless', [$this, 'safeSpaceless']),
+        ];
+    }
+
+    public function safeSpaceless($content)
+    {
+        return trim(preg_replace('/>\s+</', '><', $content ?? ''));
+    }
+
+}
